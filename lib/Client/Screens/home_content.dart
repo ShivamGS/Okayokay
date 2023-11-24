d24b77480572c510f7c3c024b134d870d1dc7bda
Akshay <81340398+AkshayBargaje@users.noreply.github.com>
2023-11-24 16:21:47 +0530

    faltu

--- lib/Client/Screens/home_content.dart
+++ lib/Client/Screens/home_content.dart
@@ -1,13 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../Auth/provider/provider.dart';
 
-class HomeContent extends StatelessWidget {
+class HomeContent extends StatefulWidget {
+  @override
+  State<HomeContent> createState() => _HomeContentState();
+}
+
+class _HomeContentState extends State<HomeContent> {
   @override
   Widget build(BuildContext context) {
     final userProvider = Provider.of<UserProvider>(context);
 
-    return Center(child: Text(userProvider.getuser()!.uid));
+    return Center(
+        child: StreamBuilder<DocumentSnapshot>(
+      stream: FirebaseFirestore.instance
+          .collection('Users')
+          .doc(userProvider.user!.uid)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text('Error: ${snapshot.error}'),
+          );
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (!snapshot.hasData || !snapshot.data!.exists) {
+          return Center(
+            child: Text('User not found'),
+          );
+        }
+
+        // Extract user data from the snapshot
+        Map<String, dynamic> userData =
+            snapshot.data!.data() as Map<String, dynamic>;
+
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('Name: ${userData['name']}'),
+            Text('Email: ${userData['email']}'),
+            // Add more fields as needed
+          ],
+        );
+      },
+    ));
   }
 }
